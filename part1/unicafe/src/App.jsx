ec6d45adfcb901b5ce7cc32632c9d25dc7642187
Jhumel Bonganciso <theodinprojectjhumel@gmail.com>
2023-12-10 14:25:10 +0800

    completed unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,21 +5,48 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const handleGoodClicks = () => {
     const newGoodClicks = good + 1;
+    const newTotal = total + 1;
     setGood(newGoodClicks);
+    setTotal(newTotal);
+    handleAverageCalculations(newTotal, newGoodClicks, bad);
+    handlePositive(newGoodClicks, newTotal)
   }
 
   const handleNeutralClicks = () => {
     const newNeutralClicks = neutral + 1;
+    const newTotal = total + 1;
+    setTotal(newTotal)
     setNeutral(newNeutralClicks);
+    handlePositive(good, newTotal);
   }
 
   const handlebadClicks = () => {
     const newBadClicks = bad + 1;
+    const newTotal = total + 1;
     setBad(newBadClicks);
+    setTotal(newTotal)
+    handleAverageCalculations(newTotal, good, newBadClicks);
+    handlePositive(good, newTotal);
   }
+
+  const handleAverageCalculations = (newTotal, good, bad) => {
+    const sumOfScores = good - bad;
+    const newAverage = sumOfScores / newTotal;
+    setAverage(newAverage);
+  }
+
+  const handlePositive = (newGood, newTotal) => {
+    const newPositive = (newGood / newTotal) * 100;
+    setPositive(newPositive);
+  } 
+
+  
 
   return (
     <div>
@@ -35,6 +62,9 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>total {total}</p>
+        <p>average {average}</p>
+        <p>positive {positive}%</p>
       </div>
     </div>
   )
