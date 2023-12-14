bd5a51238c38699ac8dd83ef0edeae94e3dad0f3
Jhumel Bonganciso <theodinprojectjhumel@gmail.com>
2023-12-14 21:52:52 +0800

    added codes for middleware lecture

--- part3/1.nodejs-and-express/index.js
+++ part3/1.nodejs-and-express/index.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+app.use(requestLogger)
+
 let notes = [
   {
     id: 1,
