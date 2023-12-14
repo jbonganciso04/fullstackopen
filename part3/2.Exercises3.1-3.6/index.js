7f5e793a5cc87bb89a7f8be2bcc2b73494ca2465
Jhumel Bonganciso <theodinprojectjhumel@gmail.com>
2023-12-14 20:45:16 +0800

    Done exercises 3-1-3.6

---
+++ part3/2.Exercises3.1-3.6/index.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const app = express();
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+
+app.get("/api/persons", (request, response) => {
+    response.json(persons)
+})
+
+app.get("/info", (request, response) => {
+    
+    response.send(
+    `<p>Phonebook has info for ${persons.length}</p>
+    <br />
+    <p>${new Date().toString()}</p> `   
+    )
+})
+
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find(p => p.id === id);
+
+    if(person) {
+        response.json(person)
+    } else {
+        response.status(404).end();
+    }
+})
+
+app.delete("/api/persons/:id", (request, response) => {
+    console.log(request.params.id);
+    const id = Number(request.params.id);
+    const person = persons.find((p) => p.id === id);
+
+    if(person) {
+        persons = persons.filter(p => p.id !== id);
+        
+        response.status(204).end();
+    } else {
+        response.status(404).end();
+    }
+} )
+
+app.post("/api/persons", (request, response) => {
+    const body = request.body;
+
+    if(!body.name) {
+        response.status(404).json({
+            error: 'Name missing'
+        })
+    }
+
+
+    if(!body.number) {
+        response.status(404).json({
+            error: 'Number missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person);
+})
+
+
+const PORT = 3001;
+app.listen(PORT);
